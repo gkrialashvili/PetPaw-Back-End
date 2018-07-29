92d5682f017ef4a521dcf985a4085d600d7c780f
zuragogoladze <zuragogoladze@github.com>
2018-07-29 15:34:20 +0400

    added user list

--- PetPaw/PetPaw/Controllers/AdminController.cs
+++ PetPaw/PetPaw/Controllers/AdminController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PetPaw.Filter;
+using PetPaw.Models;
 
 namespace PetPaw.Controllers
 {
     public class AdminController : Controller
     {
+        PetPawEntities _db = new PetPawEntities();
 
         // GET: Admin
         [AdminFilter]
         public ActionResult Index()
         {
-            return View();
+            var users = _db.users.ToList();
+            return View(users);
         }
     }
 }
